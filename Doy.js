ad6f1ec9cf64cff0e51e35d72d21087c45f9cfd8
dongzhimei <dongzhimei@jeejen.com>
2017-02-23 16:13:33 +0800

    update

--- Doy.js
+++ Doy.js
@@ -3,9 +3,9 @@
  * Date  : 17/2/16
  **/
 
-import $Scope, {$rootScope} from './core/$Scope';
-import extend from './core/extend';
-import atom from './core/atom';
+import $Scope, {$rootScope} from './src/core/$Scope';
+import extend from './src/core/extend';
+import atom from './src/core/atom';
 
 export default class Doy {
     static $rootScope: $Scope = $rootScope;
