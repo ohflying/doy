20722358352120b163f54c2245ff8781f86869a3
dongzhimei <dongzhimei@jeejen.com>
2017-03-10 11:02:24 +0800

    fix clone a map/set error
    The RN throw a error when assign a map/set object in DEV mode. so,
    delete the Object.assign

--- src/utils/shallowClone.js
+++ src/utils/shallowClone.js
@@ -11,7 +11,7 @@ export default function shallowClone(obj) {
     let className = obj.constructor ? obj.constructor.name : '';
 
     if (className == 'Set' || className == 'Map') {
-        return Object.assign(new obj.constructor(obj), obj);
+        return new obj.constructor(obj);
     } else {
         return Object.assign(new obj.constructor, obj);
     }
